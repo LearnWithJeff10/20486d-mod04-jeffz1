4d8a1bb393cc9e80c326516a6faf2dced05ae98d
Jeff <jm@jeffz.com>
2020-06-11 21:51:49 -0400

    Complete though Mod04-L02-S10

---
+++ Democode/02_RoutesExample_begin/RoutesExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutesExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(int id=50)
+        {
+            return Content("This is the Home controller with default param: " + id);
+        }
+
+        [Route("Hello/{firstname}/{lastname}")]
+        public IActionResult Greeting(string firstname, string lastname)
+        {
+            return Content($"Hello {firstname} {lastname} from module 4 demo 2");
+        }
+    }
+}
