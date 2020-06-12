3e5c2bd69dfe9d11f48f86270bc49c417adcc738
Jeff <jm@jeffz.com>
2020-06-11 20:13:14 -0400

    Finished up to Module 04 - Lesson 01 - Step 10

---
+++ Democode/01_ControllersExample_begin/ControllersExample/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ControllersExample.Models;
+
+namespace ControllersExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ExampleModel model = new ExampleModel()
+            {
+                Sentence = "Welcome to module 4 demo 1"
+            };
+            return View(model);
+        }
+    }
+}
